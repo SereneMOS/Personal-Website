8625748ed194896ae967119a5594214eb1c95624
Rosaline Flowers <rosalineaflow@gmail.com>
2024-07-16 17:05:02 -0400

    Created a new class for writing posts.

---
+++ writingpost.js
@@ -0,0 +1,10 @@
+class writingPost {
+    constructor (type, title, date, content) {
+        this.type = type;
+        this.title = title;
+        this.date = date;
+        this.content = content;
+    }
+    
+    
+}
