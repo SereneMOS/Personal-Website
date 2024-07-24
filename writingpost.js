e72b74d5d1912f0af3c6dba83fe63ee805706af1
Rosaline Flowers <rosalineaflow@gmail.com>
2024-07-24 01:10:58 -0400

    Partially implemented filter for writing posts.

--- writingpost.js
+++ writingpost.js
@@ -8,18 +8,31 @@ class writingPost extends HTMLElement {
         this.date = date
         this.content = content
     }
-    
-
 }
 
 function select(id) {
-    const post = document.querySelector(id)
+    const post = document.querySelector(id);
     if (post.style.width == "25%") {
         post.style.width = "100%";
         post.style.height = "500px";
     } else {
         post.style.width = "25%";
         post.style.height = "250px";
+    }
+}
+
+function filter(type) {
+    var i, x;
+    x = document.getElementsByClassName("post");
+
+    for (i = 0; i < x.length; i++) {
+        var m = x[i];
+        var p = m.className.split(" ");
+        if (p[1] == type) {
+            m.style.display = "initial";
+        } else {
+            m.style.display = "none";
+        }
     }
 }
 
