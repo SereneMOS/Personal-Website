3f04a3ac0063489bc5a0eb2f752b309e8c5f882f
Rosaline Flowers <rosalineaflow@gmail.com>
2024-11-04 02:06:37 -0500

    Added content to katie.html and created passwordvalidation.js for verifying password inputs.

---
+++ passwordvalidation.js
@@ -0,0 +1,14 @@
+class passwordValidation extends HTMLElement {
+    constructor (password) {
+        super()
+        this.password == password;
+    }
+}
+
+function validate(attempt, id) {
+    const element = document.querySelector(id)
+    if (attempt == password) {
+        
+    }
+}
+customElements.define("password-validation", passwordValidation)
