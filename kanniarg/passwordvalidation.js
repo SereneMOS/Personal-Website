892014bd9b2f9670c9b0f9c856e9bbaf7e3a27c6
Rosaline Flowers <rosalineaflow@gmail.com>
2024-11-11 23:46:49 -0500

    Added a new writing post, updated kanniarg content, and moved kanniarg link in home to the footer div

--- kanniarg/passwordvalidation.js
+++ kanniarg/passwordvalidation.js
@@ -5,12 +5,24 @@ class passwordValidation extends HTMLElement {
     }
 }
 
-function validate() {
+function validate1() {
     const attempt = document.getElementById('password');
     const correctPassword = 'hi';
     if (attempt.value == correctPassword) {
-        const fart = document.getElementById('veil');
-        fart.style.contentVisibility = "visible";
+        const content = document.getElementById('veil');
+        content.style.contentVisibility = "visible";
+    } else {
+        attempt.style.backgroundColor = "red";
+    }
+}
+function validate2() {
+    const attempt = document.getElementById('password');
+    const correctPassword = 'what#the#freak';
+    if (attempt.value == correctPassword) {
+        const content = document.getElementById('veil');
+        content.style.contentVisibility = "visible";
+    } else {
+        attempt.style.backgroundColor = "red";
     }
 }
 customElements.define("password-validation", passwordValidation)
